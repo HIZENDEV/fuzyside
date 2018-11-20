dc9d63c76d888ac75747e4c9b0794bd23200a473
Matthieu <contact@hizen.fr>
2018-11-20 16:44:13 +0100

    feat: camera overlay

--- App.js
+++ App.js
@@ -6,14 +6,15 @@ import {
   Text,
   TouchableOpacity,
   View,
-  Image
+  Image,
+  StatusBar
 } from 'react-native'
 // import Reactotron from 'reactotron-react-native'
 // import './ReactotronConfig'
 import { RNCamera } from 'react-native-camera'
 import Icon from 'react-native-vector-icons/Ionicons'
 
-class App extends React.Component {
+export default class App extends React.Component {
 
   takePicture = async function() {
     if (this.camera) {
@@ -26,54 +27,64 @@ class App extends React.Component {
   render() {
     return (
       <React.Fragment>
-      <RNCamera
-        ref={ref => {
-          this.camera = ref
-        }}
-        style={styles.preview}
-        type={RNCamera.Constants.Type.back}
-        flashMode={RNCamera.Constants.FlashMode.off}
-        permissionDialogTitle={'Permission to use camera'}
-        permissionDialogMessage={'We need your permission to use your camera phone'}
-      />
-      <View style={styles.overlay}>
-        <View style={styles.container}>
-        <View style={styles.overlay}>
-        <View style={styles.close}>
-        <TouchableOpacity>
-        <Icon name="ios-close" size={40} color="#FFF" />
-        </TouchableOpacity>
+        <StatusBar
+          backgroundColor="transparent"
+          barStyle="light-content"
+          translucent={true}
+        />
+        <RNCamera
+          ref={ref => {
+            this.camera = ref
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.off}
+          permissionDialogTitle={'Permission to use camera'}
+          permissionDialogMessage={'We need your permission to use your camera phone'}
+        />
+        <View style={styles.hover}>
+          <View style={styles.container}>
+            <View style={styles.overlay}>
+
+              <View style={styles.close}>
+                <TouchableOpacity>
+                  <Icon name="ios-close" size={40} color="#FFF" />
+                </TouchableOpacity>
+              </View>
+
+              <View  style={styles.actionSide}>
+                <TouchableOpacity style={styles.button}>
+                  <Icon name="ios-sync" size={24} color="#FFF" />
+                  <Text style={styles.ActionName}>Turn</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}>
+                  <Icon name="ios-timer" size={24} color="#FFF" />
+                  <Text style={styles.ActionName}>Time</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}>
+                  <Icon name="ios-color-filter" size={24} color="#FFF" />
+                  <Text style={styles.ActionName}>Filter</Text>
+                </TouchableOpacity>
+              </View>
+
+            </View>
+
+            <View style={styles.shootPicture}>
+              <TouchableOpacity style={styles.actionBottom}>
+                <Image style={styles.imgSide} source={require('./Upload.png')}/>
+                <Text style={styles.actionText}>Upload</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.actionBottom}>
+                <Image style={styles.imgShoot} source={require('./Shoot.png')}/>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.actionBottom}>
+                <Image style={styles.imgSide} source={require('./Sounds.png')}/>
+                <Text style={styles.actionText}>Sounds</Text>
+              </TouchableOpacity>
+            </View>
+
+          </View>
         </View>
-        <View  style={styles.actionSide}>
-        <TouchableOpacity style={styles.button}>
-        <Icon name="ios-sync" size={24} color="#FFF" />
-        <Text style={styles.ActionName}>Turn</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-        <Icon name="ios-timer" size={24} color="#FFF" />
-        <Text style={styles.ActionName}>Time</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-        <Icon name="ios-color-filter" size={24} color="#FFF" />
-        <Text style={styles.ActionName}>Filter</Text>
-        </TouchableOpacity>
-        </View>
-        </View>
-        <View style={styles.shootPicture}>
-        <TouchableOpacity style={styles.actionBottom}>
-        <Image style={styles.imgSide} source={require('./Upload.png')}/>
-        <Text style={styles.actionText}>Upload</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.actionBottom}>
-        <Image style={styles.imgShoot} source={require('./Shoot.png')}/>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.actionBottom}>
-        <Image style={styles.imgSide} source={require('./Sounds.png')}/>
-        <Text style={styles.actionText}>Sounds</Text>
-        </TouchableOpacity>
-        </View>
-        </View>
-      </View>
       </React.Fragment>
     )
   }
@@ -82,19 +93,23 @@ class App extends React.Component {
 // const AppIntegration = Reactotron.overlay(App)
 //
 // export default AppIntegration
-export default App
+
 
 const width = Dimensions.get('window').width
 const height = Dimensions.get('window').height
 
 const styles = StyleSheet.create({
-  overlay: {
+  hover: {
     position: 'absolute',
+    zIndex: 999,
     top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
   },
   container: {
     flex: 1,
-    marginTop: -(height - 20),
+    // marginTop: -(height - 20),
     marginBottom: 10,
     flexDirection: 'column',
     justifyContent: 'space-between',
@@ -102,13 +117,19 @@ const styles = StyleSheet.create({
   preview: {
     flex: 1,
     justifyContent: 'flex-end',
-    alignItems: 'center'
+    alignItems: 'center',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
   },
   overlay: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
-    marginHorizontal: '1.5%'
+    marginHorizontal: '1.5%',
+    marginTop: '10%'
   },
   actionSide: {
     marginTop: '6%'
@@ -147,7 +168,8 @@ const styles = StyleSheet.create({
   imgShoot: {
     width: 52,
     height: 52,
-    marginBottom: '10%'
+    marginBottom: '10%',
+    opacity: .8
   },
   imgSide: {
     width: 38,
